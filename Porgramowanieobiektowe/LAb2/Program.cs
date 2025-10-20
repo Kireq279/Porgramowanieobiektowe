d776b5618ac3d62fd9a0e8bd47eae5749f03f8ef
Jakub Sołek <71467@student.wsiz.edu.pl>
2025-10-20 14:06:59 +0200

    Lab1

---
+++ Porgramowanieobiektowe/LAb2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+namespace LAb2
+
+{
+
+
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zwierze pies = new Pies("Burek");
+            Zwierze kot = new Kot("Mruczek");
+            Zwierze waz = new Wąż("Kaa");
+            
+            Powiedzcos(kot);
+            Powiedzcos(pies);
+            Powiedzcos(waz);
+
+            Pracownik piekarz = new Piekarz();
+            piekarz.Pracuj();
+
+            A obkietA = new A();
+            B obkietB = new B();
+            C obkietC = new C();
+
+
+        }
+        static void Powiedzcos(Zwierze z)
+        {
+            z.daj_głos();
+            Console.WriteLine(z.GetType());
+        }
+    }
+}
+class Zwierze
+{
+    protected string nazwa;
+    
+    public Zwierze(string nazwa)
+    {
+        this.nazwa = nazwa;
+    }
+    public virtual void daj_głos()
+    {
+        Console.WriteLine("...");
+    }
+}
+class Pies : Zwierze
+    {
+        public Pies(string nazwa) : base(nazwa) { }
+        public override void daj_głos()
+        {
+            Console.WriteLine($"{nazwa} ; Hau Hau");
+        }
+    }
+class Kot : Zwierze
+    {
+        public Kot(string nazwa) : base(nazwa) { }
+        public override void daj_głos()
+        {
+            Console.WriteLine($"{nazwa} ; Miau Miau");
+        }
+    }
+class Wąż : Zwierze
+    {
+        public Wąż(string nazwa) : base(nazwa) { }
+        public override void daj_głos()
+        {
+            Console.WriteLine($"{nazwa} ; Sssss Sssss");
+        }
+    }
+abstract class Pracownik
+{
+    public abstract void Pracuj();
+}
+class Piekarz : Pracownik
+{
+    public override void Pracuj()
+    {
+        Console.WriteLine("Trwa pieczenie...");
+    }
+}
+class A
+{   public A()
+    {
+        Console.WriteLine("To jest konstruktor A");
+    }
+}
+class B : A
+{
+    public B() : base()
+    {
+        Console.WriteLine("To jest konstruktor B");
+    }
+}   
+class C : B
+{
+    public C() : base()
+    {
+        Console.WriteLine("To jest konstruktor C");
+    }
+}
--- Porgramowanieobiektowe/Lab1/Program.cs
+++ Porgramowanieobiektowe/Lab1/Program.cs
@@ -1,60 +1,125 @@
-﻿
+﻿using System;
+
 class Program
 {
     public static void Main()
     {
         Console.WriteLine("To jest cwiczenie 1");
 
+        zwierze[] tab = new zwierze[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"\nPodaj informacje o zwierzeciu {i + 1}:");
+
+            Console.Write("Imie: ");
+            string imie = Console.ReadLine();
+
+            Console.Write("Gatunek: ");
+            string gatunek = Console.ReadLine();
+
+            Console.Write("Liczba nog: ");
+            int nogi = int.Parse(Console.ReadLine());
+
+            tab[i] = new zwierze(imie, gatunek, nogi);
+        }
+
+        zwierze klon = new zwierze(tab[1]);
+        klon.Nazwa = "KlonowaneZwierze";
+
+        Console.WriteLine("\n--- Dane wszystkich zwierząt ---");
+
+        foreach (zwierze z in tab)
+        {
+            Wypisz(z);
+        }
+
+        Console.WriteLine("\n--- Dane klona ---");
+        Wypisz(klon);
+
+        Console.WriteLine($"\nLiczba stworzonych zwierząt: {zwierze.getliczbaZwierzat()}");
+    }
+
+    public static void Wypisz(zwierze z)
+    {
+        Console.WriteLine($"Imie: {z.Nazwa}, Gatunek: {z.getgatunek()}, Nogi: {z.getilenog()}");
+        z.daj_glos();
     }
 }
 
 class zwierze
 {
-    public string imie;
-    public string gatunek;
-    public int ilenog;
-    public static int liczbaZwierzat = 0;
+    private string imie;
+    private string gatunek;
+    private int ilenog;
 
-    public string getnazwa()
+    private static int liczbaZwierzat = 0;
+
+    public string Nazwa
     {
-        return imie;
+        get { return imie; }
+        set { imie = value; }
     }
+
     public string getgatunek()
     {
         return gatunek;
     }
+
     public int getilenog()
     {
         return ilenog;
     }
+
     public static int getliczbaZwierzat()
     {
         return liczbaZwierzat;
     }
-    public void setnazwa(string imie)
-    {
-        this.imie = imie;
-    }
-    public void setgatunek(string gatunek)
-    {
-        this.gatunek = gatunek;
-    }
-    public void setilenog(int ilenog)
-    {
-        this.ilenog = ilenog;
-    }
+
     public zwierze()
     {
         imie = "Rex";
         gatunek = "Pies";
-        ilenog = 0;
+        ilenog = 4;
         liczbaZwierzat++;
     }
+
     public zwierze(string imie, string gatunek, int ilenog)
     {
         this.imie = imie;
         this.gatunek = gatunek;
         this.ilenog = ilenog;
         liczbaZwierzat++;
+    }
+
+    public zwierze(zwierze inne)
+    {
+        this.imie = inne.imie;
+        this.gatunek = inne.gatunek;
+        this.ilenog = inne.ilenog;
+        liczbaZwierzat++;
+    }
+
+    public void daj_glos()
+    {
+        switch (gatunek.ToLower())
+        {
+            case "kot":
+                Console.WriteLine("Miau!");
+                break;
+            case "pies":
+                Console.WriteLine("Hau hau!");
+                break;
+            case "krowa":
+                Console.WriteLine("Muuu!");
+                break;
+            default:
+                Console.WriteLine("Brak przypisanego dźwięku.");
+                break;
+        }
+    }
+
+    ~zwierze()
+    {
     }
 }
